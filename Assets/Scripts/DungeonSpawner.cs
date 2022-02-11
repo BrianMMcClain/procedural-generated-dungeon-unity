1450e3d1e30e45bbccabc7e1a9250104bb2b038e
Brian McClain <brianmmcclain@gmail.com>
2022-02-11 00:44:16 -0500

    Update spawn logic

--- Assets/Scripts/DungeonSpawner.cs
+++ Assets/Scripts/DungeonSpawner.cs
@@ -2,82 +2,199 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Room
+{
+    private Room lastRoom;
+    private Room nextRoom;
+    private int x;
+    private int y;
+
+    public Room(int x, int y, Room lastRoom)
+    {
+        this.x = x;
+        this.y = y;
+        this.lastRoom = lastRoom;
+        nextRoom = null;
+    }
+
+    public Room(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.lastRoom = null;
+        nextRoom = null;
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+
+    public int GetY()
+    {
+        return y;
+    }
+
+    public Room GetLastRoom()
+    {
+        return this.lastRoom;
+    }
+
+    public Room GetNextRoom()
+    {
+        return this.nextRoom;
+    }
+
+    public void SetNextRoom(Room room)
+    {
+        this.nextRoom = room;
+    }
+
+    public bool HasNextRoom()
+    {
+        if (this.nextRoom != null)
+            return true;
+        return false;
+    }
+}
+
 public class DungeonSpawner : MonoBehaviour
 {
     public int roomCount = 5;
-    public GameObject roomPrefab;
-
-    private bool[,] layout;
+    private int roomsGenerated;
+    private Room[,] layout;
+    private List<Room> rooms;
     private int roomOffset = 10;
+
+    public GameObject roomPrefab;
 
     void Start()
     {
-        layout = new bool[roomCount * 2, roomCount * 2];
+        layout = new Room[roomCount * 2, roomCount * 2];
+        rooms = new List<Room>();
         int layoutX = roomCount;
         int layoutY = roomCount;
-        layout[layoutX, layoutX] = true;
-
-        for (int i = 1; i < roomCount; i++)
-        {
-            float xDist = 0;
-            float zDist = 0;
-
-            bool roomGenerated = false;
-            while (!roomGenerated)
-            {
-                int direction = Random.Range(1, 5);
-                Debug.Log(direction);
-                switch (direction)
-                {
-                    case 1:
-                        // Up
-                        if (layoutY > 0 && !layout[layoutX, layoutY-1])
-                        {
-                            zDist = roomOffset;
-                            layoutY--;
-                            roomGenerated = true;
-                        }
-
-                        break;
-                    case 2:
-                        // Down
-                        if (layoutY < roomCount * 2 && !layout[layoutX, layoutY+1])
-                        {
-                            zDist = -roomOffset;
-                            layoutY++;
-                            roomGenerated = true;
-                        }
-                        break;
-                    case 3:
-                        // Left
-                        if (layoutX > 0 && !layout[layoutX-1, layoutY])
-                        {
-                            xDist = -roomOffset;
-                            layoutX--;
-                            roomGenerated = true;
-                        }
-                        break;
-                    case 4:
-                        // Right
-                        if (layoutX < roomCount * 2 && !layout[layoutX+1, layoutY])
-                        {
-                            Debug.Log("Right");
-                            xDist = roomOffset;
-                            layoutX++;
-                            roomGenerated = true;
-                        }
-                        break;
-                }
-            }
-
-            layout[layoutX, layoutY] = true;
-            transform.Translate(new Vector3(xDist, 0, zDist));
-            Instantiate(roomPrefab, transform.position, transform.rotation);
-        }
+        Room startRoom = new Room(layoutX, layoutY);
+        rooms.Add(startRoom);
+        layout[layoutX, layoutX] = startRoom;
+        roomsGenerated = 1;
+        GenerateLayout(startRoom);
+        GenerateDungeon(startRoom);
+        Debug.Log(rooms.Count);
     }
 
     void Update()
     {
         
+    }
+
+    private void DebugDungeon()
+    {
+        string debugString = "";
+        for (int y = 0; y < layout.GetLength(0); y++)
+        {
+            for (int x = 0; x < layout.GetLength(1); x++)
+            {
+                if (layout[x, y] != null)
+                {
+                    debugString += "X";
+                } else
+                {
+                    debugString += "_";
+                }
+            }
+            debugString += "\n";
+        }
+        Debug.Log(debugString);
+    }
+
+    private void GenerateLayout(Room startRoom)
+    {
+        int layoutX = startRoom.GetX();
+        int layoutY = startRoom.GetY();
+        Room lastRoom = startRoom;
+
+        while (roomsGenerated < roomCount)
+        {
+            bool roomGenerated = false;
+            int tries = 0;
+
+            while (!roomGenerated && tries < 10)
+            {
+                int direction = Random.Range(0, 4);
+                int potentialX = lastRoom.GetX();
+                int potentialY = lastRoom.GetY();
+                switch (direction)
+                {
+                    case 0:
+                        // Up
+                        potentialX = lastRoom.GetX();
+                        potentialY = lastRoom.GetY() - 1;
+                        if (potentialY >= 0 && layout[potentialX, potentialY] == null)
+                        {
+                            roomGenerated = true;
+                        }
+                        break;
+                    case 1:
+                        // Down
+                        potentialX = lastRoom.GetX();
+                        potentialY = lastRoom.GetY() + 1;
+                        if (potentialY < roomCount * 2 && layout[potentialX, potentialY] == null)
+                        {
+                            roomGenerated = true;
+                        }
+                        break;
+                    case 2:
+                        // Left
+                        potentialX = lastRoom.GetX() - 1;
+                        potentialY = lastRoom.GetY();
+                        if (potentialX >= 0 && layout[potentialX, potentialY] == null)
+                        {
+                            roomGenerated = true;
+                        }
+                        break;
+                    case 3:
+                        // Right
+                        potentialX = lastRoom.GetX() + 1;
+                        potentialY = lastRoom.GetY();
+                        if (potentialX < roomCount * 2 && layout[potentialX, potentialY] == null)
+                        {
+                            roomGenerated = true;
+                        }
+                        break;
+                }
+
+                if (roomGenerated)
+                {
+                    Room newRoom = new Room(potentialX, potentialY, lastRoom);
+                    rooms.Add(newRoom);
+                    layout[potentialX, potentialY] = newRoom;
+                    lastRoom.SetNextRoom(newRoom);
+                    lastRoom = newRoom;
+                    layoutX = potentialX;
+                    layoutY = potentialY;
+                    roomsGenerated++;
+                } else
+                {
+                    tries++;
+                }
+            }
+
+            if (!roomGenerated)
+            {
+                lastRoom = lastRoom.GetLastRoom();
+            }
+        }
+    }
+
+    private void GenerateDungeon(Room startRoom)
+    {
+        Room currentRoom = startRoom;
+        while (currentRoom != null)
+        {
+            Vector3 loc = new Vector3(currentRoom.GetX() * roomOffset, 0, currentRoom.GetY() * roomOffset);
+            Instantiate(roomPrefab, loc, transform.rotation);
+            currentRoom = currentRoom.GetNextRoom();
+        }
     }
 }
